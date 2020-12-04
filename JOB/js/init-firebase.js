9a6c93a050c95d45991404c7e684fa3701f3aabc
jobclues <75485712+jobclues@users.noreply.github.com>
2020-12-04 19:46:07 +0530

    Add files via upload

---
+++ JOB/js/init-firebase.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDO0K5faGmi7KBsKNodWX-K0k-y8AGzjtU",
+  authDomain: "dem11-8c9a0.firebaseapp.com",
+  databaseURL: "https://dem11-8c9a0.firebaseio.com",
+  projectId: "dem11-8c9a0",
+  storageBucket: "dem11-8c9a0.appspot.com",
+  messagingSenderId: "757666753988",
+  appId: "1:757666753988:web:b7ee56483864c9084a0891",
+  measurementId: "G-4JKV3J6NPQ"
+};
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
